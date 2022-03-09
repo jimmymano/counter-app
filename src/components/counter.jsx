430b9307bb1c1f785404a87ecb15de7fa23f6280
Jimmy Jam <jimmymano@mail.com>
2022-03-09 00:56:39 -0600

    implemented increment with setState binding event handler

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,19 +3,27 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 1,
-    tags: ["tag1", "tag2", "tag3"],
+  };
+
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map((tag) => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
       </React.Fragment>
     );
   }
