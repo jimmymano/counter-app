984d7aebb56c3cadcb5fba7ded70c760a5c11456
Jimmy Jam <jimmymano@mail.com>
2022-03-15 06:03:15 -0500

    set Counter state with props

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,25 +2,26 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
+    value: this.props.value,
   };
   //1.This method returns a string of Zero or the count property
   formatCount() {
-    const { count } = this.state;
+    const { value: count } = this.state;
     return count === 0 ? "Zero" : count;
   }
   //2.This method will apply classes based on state property
   getBadgeClasses() {
     let classes = "badge m-2";
-    classes += this.state.count === 0 ? " bg-warning" : " bg-primary";
+    classes += this.state.value === 0 ? " bg-warning" : " bg-primary";
     return classes;
   }
   //3.This method will increment by using setState method
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   render() {
+    console.log("props", this.props);
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
