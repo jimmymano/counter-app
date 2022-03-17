8d6de3dca1ba52780a78779b70c4c25fc847dd1b
Jimmy Jam <jimmymano@mail.com>
2022-03-16 23:34:35 -0500

    added delete functionality

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    value: this.props.value,
+    value: this.props.counter.value,
   };
   //1.This method returns a string of Zero or the count property
   formatCount() {
@@ -32,7 +32,7 @@ class Counter extends Component {
           Increment
         </button>
         <button
-          onClick={this.props.onDelete}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className="btn bg-danger btn-sm m-2"
         >
           Delete
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,14 +5,17 @@ class Counters extends Component {
   state = {
     counters: [
       { id: 1, value: 4 },
-      { id: 2, value: 0 },
+      { id: 2, value: 3 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
     ],
   };
-
-  handleDelete = () => {
-    console.log("Event handler called");
+  //Question for Kevin. How is the handleDelete function passing the state as a parameter?
+  handleDelete = (counterId) => {
+    console.log("Event handler called", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    console.log(counters);
+    this.setState({ counters });
   };
 
   render() {
@@ -21,8 +24,7 @@ class Counters extends Component {
         {this.state.counters.map((counter) => (
           <Counter
             key={counter.id}
-            value={counter.value}
-            id={counter.id}
+            counter={counter}
             onDelete={this.handleDelete}
           />
         ))}
