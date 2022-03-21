25a374d05b5760c04b3fbdd1ad870ac61ac5d632
Jimmy Jam <jimmymano@mail.com>
2022-03-21 05:14:49 -0500

    lifted state to app.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,54 @@ import NavBar from "./components/navbar";
 import "./App.css";
 import Counters from "./components/counters";
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar />
-      <main className="container">
-        <Counters />
-      </main>
-    </div>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 3 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    console.log(this.state.counters[index]);
+    this.setState({ counters });
+  };
+  //Question for Kevin. How is the handleDelete function passing the state as a parameter?
+  handleDelete = (counterId) => {
+    console.log("Event handler called", counterId);
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    console.log(counters);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar />
+        <main className="container">
+          <Counters
+            counters={this.state.counters}
+            onReset={this.handleReset}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+          />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,53 +2,21 @@ import React, { Component } from "react";
 import Counter from "./counter";
 
 class Counters extends Component {
-  state = {
-    counters: [
-      { id: 1, value: 4 },
-      { id: 2, value: 3 },
-      { id: 3, value: 0 },
-      { id: 4, value: 0 },
-    ],
-  };
-  handleIncrement = (counter) => {
-    const counters = [...this.state.counters];
-    const index = counters.indexOf(counter);
-    counters[index] = { ...counter };
-    counters[index].value++;
-    console.log(this.state.counters[index]);
-    this.setState({ counters });
-  };
-  //Question for Kevin. How is the handleDelete function passing the state as a parameter?
-  handleDelete = (counterId) => {
-    console.log("Event handler called", counterId);
-    const counters = this.state.counters.filter((c) => c.id !== counterId);
-    console.log(counters);
-    this.setState({ counters });
-  };
-
-  handleReset = () => {
-    const counters = this.state.counters.map((c) => {
-      c.value = 0;
-      return c;
-    });
-    this.setState({ counters });
-  };
-
   render() {
     return (
       <div>
         <button
-          onClick={this.handleReset}
+          onClick={this.props.onReset}
           className="btn bg-primary btn-sm m-2"
         >
           Reset
         </button>
-        {this.state.counters.map((counter) => (
+        {this.props.counters.map((counter) => (
           <Counter
             key={counter.id}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
             counter={counter}
-            onDelete={this.handleDelete}
-            onIncrement={this.handleIncrement}
           />
         ))}
       </div>
