478895587fc1cef647ca78ad48994e5863ae4c2e
Jimmy Jam <jimmymano@mail.com>
2022-03-22 13:42:18 -0500

    disabled decrement button when value is zero

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -41,12 +41,13 @@ class Counter extends Component {
           <button
             onClick={() => this.props.onDecrement(this.props.counter)}
             className="btn btn-secondary btn-sm m-2"
+            disabled={this.props.counter.value === 0 ? "disable" : ""}
           >
             -
           </button>
           <button
             onClick={() => this.props.onDelete(this.props.counter.id)}
-            className="btn bg-danger btn-sm m-2"
+            className="btn bg-danger btn-sm "
           >
             Delete
           </button>
